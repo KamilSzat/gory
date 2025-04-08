105f8a7b539192af5ec82e90259d94cccce1c9f7
KamilSzat <uT_3776@zsp3zamosc.pl>
2025-04-08 12:42:11 +0200

    Zadanie na ocenę

---
+++ MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace gory
+{
+    public partial class MainPage : ContentPage
+    {
+        int label = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OnSaveClicked(object sender, EventArgs e)
+        {
+            label=+0;
+            if (label == 1)
+                label2.Text = $"{label} polubienie";
+            else
+                label2.Text = $"{label} polubień";
+
+            SemanticScreenReader.Announce(label2.Text);
+        }
+     
+        private void OnDelateClicked(object sender, EventArgs e)
+        {
+
+            label--;
+            if (label <= 1)
+                label2.Text = $"{label} polubienie";
+            else
+                label2.Text = $"{label} polubień";
+
+            SemanticScreenReader.Announce(label2.Text);
+        }
+        private void OnLikeClicked(object sender, EventArgs e)
+        {
+            label++;
+            if (label == 1)
+                label2.Text = $"{label} polubienie";
+            else
+                label2.Text = $"{label} polubień";
+
+            SemanticScreenReader.Announce(label2.Text);
+        }
+    }
+
+}
